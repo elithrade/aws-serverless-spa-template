57fbc0d1b3ff8d452d9da8297342bd1839ae6500
Bin Hu <elithrade@gmail.com>
2021-11-28 16:08:30 +0800

    feat/ add comments to SpaEdgeFunction

--- cdk/src/SpaEdgeFunction.ts
+++ cdk/src/SpaEdgeFunction.ts
@@ -3,6 +3,20 @@ import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as core from '@aws-cdk/core';
 import { EdgeFunctionProps } from '@aws-cdk/aws-cloudfront/lib/experimental';
 
+/**
+ * The SpaEdgeFunction construct provides a lambda function for CloudFront
+ * Distribution lambdaFunctionAssociations for origin requests. For each
+ * request, if the request does not look like a file changes the uri for the
+ * request to `/index.html`.
+ *
+ * Requests are assumed to be file request if and only if they contain a `.` and
+ * no `/` occur after the last instance of a `.`.  e.g. `styles/main.css` would
+ * be treated as a file, but `projects/customer.last/` would not be treated as a
+ * files.
+ *
+ * This is to aid deploying web apps employing browser routing instead of hash
+ * routing.
+ */
 export class SpaEdgeFunction extends cloudfront.experimental.EdgeFunction {
   constructor(scope: core.Construct, id: string, props?: EdgeFunctionProps) {
     super(scope, id, {
